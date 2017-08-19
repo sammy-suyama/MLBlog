a4f32f39c364b5b6d0731477c781d1407e9d9a9f
sammy <ashsuit@gmail.com>
2017-08-19 20:53:12 +0900

    modified variable names.

--- src/change_point.jl
+++ src/change_point.jl
@@ -9,8 +9,8 @@ PyDict(matplotlib["rcParams"])["font.family"] = "TakaoPGothic"
 
 test = false
 
-function VI(X, a1, b1, a2, b2, phi, max_iter)
-    N = size(X, 1)
+function VI(C, a1, b1, a2, b2, phi, max_iter)
+    N = size(C, 1)
     phi_h = deepcopy(phi)
 
     a1_h = NaN
@@ -26,17 +26,17 @@ function VI(X, a1, b1, a2, b2, phi, max_iter)
         #tmp = cumsum(phi_h)
         #delta2 = cat(1, 0, tmp[1:end-1])
         delta2 = cumsum(phi_h)
-        a1_h = a1 + sum(X.*delta1)
+        a1_h = a1 + sum(C.*delta1)
         b1_h = b1 + sum(delta1)
-        a2_h = a2 + sum(X.*delta2)
+        a2_h = a2 + sum(C.*delta2)
         b2_h = b2 + sum(delta2)
 
-        # update s
+        # update tau
         ln_p1 = Array{Float64, 1}(N)
         ln_p2 = Array{Float64, 1}(N)
         for n in 1 : N
-            ln_p1[n] = X[n] * (digamma(a1_h) - log(b1_h)) - a1_h/b1_h
-            ln_p2[n] = X[n] * (digamma(a2_h) - log(b2_h)) - a2_h/b2_h
+            ln_p1[n] = C[n] * (digamma(a1_h) - log(b1_h)) - a1_h/b1_h
+            ln_p2[n] = C[n] * (digamma(a2_h) - log(b2_h)) - a2_h/b2_h
         end
 
         ln_tmp = Array{Float64, 1}(N)
@@ -63,13 +63,13 @@ if test
     # generate sample
     lambda1 = rand(Gamma(a1, 1.0/b1))
     lambda2 = rand(Gamma(a2, 1.0/b2))
-    s = rand(Categorical(phi))
-    X = Array{Float64, 1}(N)
-    for n in 1 : s
-        X[n] = rand(Poisson(lambda1))
+    tau = rand(Categorical(phi))
+    C = Array{Float64, 1}(N)
+    for n in 1 : tau
+        C[n] = rand(Poisson(lambda1))
     end
-    for n in s+1 : N
-        X[n] = rand(Poisson(lambda2))
+    for n in tau+1 : N
+        C[n] = rand(Poisson(lambda2))
     end
 else
     data_url = "https://git.io/vXTVC"
@@ -87,9 +87,9 @@ else
         data = readcsv(IOBuffer(res.data))
         writecsv(dir_name * "/" * file_name, data)
     end
-    X = data[:,1]
+    C = data[:,1]
 
-    N = size(X, 1)
+    N = size(C, 1)
     phi = ones(N) / N
 end
 
@@ -99,24 +99,24 @@ end
 if test
     figure("data samples")
     clf()
-    bar(1:N, X, color="#348ABD")
-    plot([1, s], [lambda1, lambda1], "g--")
-    plot([s+1, N], [lambda2, lambda2], "g--")
-    plot([s+0.5, s+0.5],[0, maximum(X)],"r--")
+    bar(1:N, C, color="#348ABD")
+    plot([1, tau], [lambda1, lambda1], "g--")
+    plot([tau+1, N], [lambda2, lambda2], "g--")
+    plot([tau+0.5, tau+0.5],[0, maximum(C)],"r--")
     xlim([0, N])
 else
     figure("data samples")
     clf()
-    bar(1:N, X, color="#348ABD")
+    bar(1:N, C, color="#348ABD")
     xlim([0, N])
 end
 
 # inference
 max_iter = 100
-phi_est, lambda1_est, lambda2_est = VI(X, a1, b1, a2, b2, phi, max_iter)
+phi_est, lambda1_est, lambda2_est = VI(C, a1, b1, a2, b2, phi, max_iter)
 
 # calc expectation
-s_expt = sum(phi_est .* range(1,N))
+tau_expt = sum(phi_est .* range(1,N))
 lambda_est = Array{Float64, 1}(N)
 for n in 1 : N
     tmp = sum(phi_est[1:n])
@@ -126,7 +126,7 @@ end
 figure("results")
 clf()
 subplot(2,1,1)
-bar(1:N, X, color="#348ABD", label="受信数")
+bar(1:N, C, color="#348ABD", label="受信数")
 plot(1:N, lambda_est, color="#E24A33", label="期待値")
 xlim([0, N])
 ylabel("受信メッセージ数")
@@ -134,7 +134,7 @@ xlabel("経過日数")
 legend()
 subplot(2,1,2)
 bar(1:N, phi_est, color="#348ABD", label="事後確率")
-plot([s_expt, s_expt], [0, maximum(phi_est)], "r--", label="期待値")
+plot([tau_expt, tau_expt], [0, maximum(phi_est)], "r--", label="期待値")
 xlim([0, N])
 ylabel("事後確率")
 xlabel("経過日数")
